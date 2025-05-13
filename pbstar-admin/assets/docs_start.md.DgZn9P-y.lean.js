95b2e01f2037e7c975fdeb7e9f65db63a7d9b8ef
pengbo <pbstar@sina.cn>
2025-05-13 21:03:26 +0800

    文档

--- pbstar-admin/assets/docs_start.md.DgZn9P-y.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as i,c as a,o as n,ae as h}from"./chunks/framework.C0tAYdfm.js";const g=JSON.parse('{"title":"快速开始","description":"","frontmatter":{},"headers":[],"relativePath":"docs/start.md","filePath":"docs/start.md"}'),t={name:"docs/start.md"};function l(p,s,k,e,r,d){return n(),a("div",null,s[0]||(s[0]=[h("",9)]))}const c=i(t,[["render",l]]);export{g as __pageData,c as default};
